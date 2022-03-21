750505baa3919e8d3551aba1d2830adf1e3ea4b4
jastis <jemilehinadewale@yahoo.com>
2022-03-21 22:30:28 +0100

    updated env.php

--- env.php
+++ env.php
@@ -22,20 +22,7 @@ $settings['db']['name'] = 'tconnect_db';
 $settings['db']['username'] = 'root';
 $settings['db']['password'] = '';
 
-// $settings['db']['host'] = 'teekonect-server.mysql.database.azure.com:3306';
-// $settings['db']['name'] = 'trausox1_tkonect_db';
-// $settings['db']['username'] = 'trausox';
-// $settings['db']['password'] = 'Thedirector@1';
 
-// $settings['smtp']['username'] = 'donotreply@trausox.com';
-// $settings['smtp']['password'] = '[gEW]tR!UcTa';
-
-// $settings['security']['secretKey'] = 'bDS6lzFqvvSQ8ALbOxatm7(Vk7mLQyzqaS34Q4oR1ew=';
-// $settings['security']['serverName'] = 'www.trausox.com';
-
-// $settings['sms']['asid'] = 'AC2c5791d5bf5e1ff3e207fc1fce9fcb7c';
-// $settings['sms']['token'] = 'QUMyYzU3OTFkNWJmNWUxZmYzZTIwN2ZjMWZjZTlmY2I3YzoyNjYxYTE4YWEwZWI2MWRlMzA0YjA1YzA1YjdmZTAzZA==';
-// $settings['sms']['msid'] = 'MG38881ba865c86ec9fa569920a5864997';
 
 $settings['cost']=[
     'customCard'=> 2,
